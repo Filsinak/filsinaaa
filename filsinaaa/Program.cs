16e78649d5c7663b71ed3a560074f29462ca0e72
Катерина Фильшина <filsinakk@mail.ru>
2021-11-27 15:40:38 +0300

    Добавьте файлы проекта.

---
+++ filsinaaa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace filsinaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
